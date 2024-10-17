1796462847503d0804596786ce6e738c785639b6
anasrazaweb <anasrazadev@gmail.com>
2024-10-17 20:51:11 +0500

    hotel-info  create checkBox

--- src/screens/hotel-info/components/amenities-modal.jsx
+++ src/screens/hotel-info/components/amenities-modal.jsx
@@ -1,24 +1,55 @@
 import React from "react";
-
 import * as yup from "yup";
-
 import Modal from "@/src/components/modal";
 import { RHFFormProvider } from "@/src/components/hook-form";
 import { useForm } from "react-hook-form";
 import RHFAutoComplete from "@/src/components/hook-form/rhf-auto-complete";
 import { yupResolver } from "@hookform/resolvers/yup";
+import {
+  LocalStorageGetItem,
+  LocalStorageSetItem,
+} from "@/src/utils/localstorage";
 
-const AmenitiesModal = ({ isOpen, onClose }) => {
+const AmenitiesModal = ({ isOpen, onClose, setRefetch }) => {
   const schema = yup.object({
-    amenities: yup.array().optional(),
+    amenities: yup.array().optional(), // Add any validation rules you want
   });
 
-  const methods = useForm({ resolver: yupResolver(schema) });
+  // Retrieve stored data from localStorage
+  let mainSettings = {};
+  try {
+    const storedData = LocalStorageGetItem("amenities");
+    mainSettings = storedData ? JSON.parse(storedData) : {};
+    setRefetch(isOpen);
+  } catch (error) {
+    console.error("Error parsing stored data", error);
+  }
+
+  // Initialize form with existing amenities data
+  const methods = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: mainSettings, // Use saved data for default values
+  });
 
   const { handleSubmit } = methods;
 
   const onSubmit = handleSubmit(async (data) => {
-    console.log("data amenities", data);
+    try {
+      const storedData = LocalStorageGetItem("amenities");
+      let existingData = storedData ? JSON.parse(storedData) : {};
+
+      const updatedData = {
+        ...existingData,
+        amenities: [
+          ...(existingData.amenities || []),
+          ...(data.amenities || []),
+        ],
+      };
+
+      LocalStorageSetItem("amenities", JSON.stringify(updatedData));
+    } finally {
+      onClose();
+    }
   });
 
   return (
