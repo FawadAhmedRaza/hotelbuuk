52c3dac6abff6a44eb8204a6223ad0ed3a7f146e
Rabeet-5 <rabeetsati@gmail.com>
2024-10-17 21:22:02 +0500

    updates in hotel dashboard section

---
+++ src/screens/hotel-dashboard/sections/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Pannel } from "@/src/components";
+import HotelCards from "./components/cards";
+
+const HotelDashboardSections = () => {
+  return (
+    <Pannel>
+      <HotelCards />
+    </Pannel>
+  );
+};
+
+export default HotelDashboardSections;
