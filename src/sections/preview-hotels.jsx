a9f1c48560c12981baa0d18a2a91898f96f419bd
anasrazaweb <anasrazadev@gmail.com>
2024-11-02 00:38:52 +0500

    add home page Skeleton

--- src/sections/preview-hotels.jsx
+++ src/sections/preview-hotels.jsx
@@ -1,138 +1,73 @@
 "use client";
-import React, { useEffect, useRef, useState } from "react";
-
-// Components and Others...
-import { hotels } from "../_mock/_hotels";
-import {
-  AnchorTag,
-  BgIcon,
-  HotelCard,
-  Iconify,
-  Typography,
-} from "../components";
-
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
-import "../app/globals.css";
+import React, { useEffect, useState } from "react";
+import { HotelCard } from "../components";
 import { useDispatch, useSelector } from "react-redux";
-import { getAllNomadEvents } from "../redux/events/thunk";
-import { getAllEvents } from "../redux/all-events/thunk";
 import HotelCardSkeleton from "../components/Skeleton/hotel-card-skeleton";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../components/ui/carousel";
+import { getAllEvents } from "../redux/all-events/thunk";
+// import { getAllEvents } from "../redux/actions"; // Adjust if necessary
 
 export const PreviewHotels = () => {
   const swiperRef = React.useRef(null);
-
-  const [isPrevDisabled, setIsPrevDisabled] = useState(true);
-  const [isNextDisabled, setIsNextDisabled] = useState(false);
   const dispatch = useDispatch();
-
   const { events, isLoading } = useSelector((state) => state.allEvents);
 
-  const updateNavigation = () => {
-    if (swiperRef.current) {
-      const { isBeginning, isEnd } = swiperRef.current.swiper;
-
-      setIsPrevDisabled(isBeginning); // Disable left button if at the beginning
-      setIsNextDisabled(isEnd); // Disable right button if at the end
-    }
-  };
+  const [isEventLoading, setIsEventLoading] = useState(true);
 
   useEffect(() => {
-    if (swiperRef.current) {
-      swiperRef?.current?.navigation?.update();
-    }
-  }, [swiperRef]);
-
-  useEffect(() => {
-    async function fetchEvents() {
-      await dispatch(getAllEvents()).unwrap();
-    }
+    const fetchEvents = async () => {
+      setIsEventLoading(true);
+      try {
+        await dispatch(getAllEvents()).unwrap();
+      } catch (error) {
+        console.error("Failed to fetch events:", error);
+      } finally {
+        setIsEventLoading(false);
+      }
+    };
 
     fetchEvents();
-  }, []);
+  }, [dispatch]);
 
   return (
-    <div className="w-full relative px-10">
-      {/* Left Arrow Button */}
-      <span
-        className={`swiper-button-prev custom-prev ${
-          isPrevDisabled ? "!opacity-50 !cursor-not-allowed" : ""
-        }`}
-        onClick={() => {
-          swiperRef.current?.swiper?.slidePrev();
-          updateNavigation();
-        }}
-        aria-disabled={isPrevDisabled}
-      >
-        <Iconify iconName="cuida:arrow-left-outline" />
-      </span>
-
-      {/* Right Arrow Button */}
-      <span
-        className={`swiper-button-next custom-next ${
-          isNextDisabled ? "opacity-50 cursor-not-allowed" : ""
-        }`}
-        onClick={() => {
-          swiperRef.current?.swiper?.slideNext();
-          updateNavigation();
-        }}
-      >
-        <Iconify iconName="cuida:arrow-right-outline" />
-      </span>
-
-      <Swiper
-        ref={swiperRef}
-        spaceBetween={10}
-        slidesPerView={4}
-        modules={[Navigation]}
-        breakpoints={{
-          0: {
-            slidesPerView: 1,
-            spaceBetween: 10,
-          },
-          700: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-          },
-          950: {
-            slidesPerView: 3,
-            spaceBetween: 20,
-          },
-          1200: {
-            slidesPerView: 4,
-            spaceBetween: 10,
-          },
-        }}
-      >
-        {/* {events?.map((event) => (
-          <SwiperSlide key={event.id} className="flex flex-col">
-            {isLoading ? (
-              <HotelCardSkeleton />
-            ) : (
-              <HotelCard event={event} className="" />
-            )}
-          </SwiperSlide>
-        ))} */}
-
-        {isLoading
-          ? [...Array(4)].map(
-              (
-                _,
-                index // Show 4 skeletons if loading
-              ) => (
-                <SwiperSlide key={index} className="flex flex-col">
-                  <HotelCardSkeleton />
-                </SwiperSlide>
-              )
-            )
-          : events?.map((event) => (
-              <SwiperSlide key={event.id} className="flex flex-col">
-                <HotelCard event={event} />
-              </SwiperSlide>
+    <div className="w-full relative">
+      {isLoading || isEventLoading ? (
+        <Carousel className="px-3">
+          <CarouselContent>
+            {[...Array(4)].map((_, index) => (
+              <CarouselItem
+                key={index}
+                className="sm:basis-1/2 md:basis-1/3 lg:basis-1/4"
+              >
+                <HotelCardSkeleton />
+              </CarouselItem>
             ))}
-      </Swiper>
+          </CarouselContent>
+        </Carousel>
+      ) : (
+        <Carousel className="px-3">
+          <CarouselContent>
+            {events?.map((event, index) => (
+              <CarouselItem
+                key={index}
+                className="pl-5 sm:basis-1/2 md:basis-1/3 lg:basis-1/4"
+              >
+                <div className="p-0">
+                  <HotelCard event={event} />
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      )}
     </div>
   );
 };
