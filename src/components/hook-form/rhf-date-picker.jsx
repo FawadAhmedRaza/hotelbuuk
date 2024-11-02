b0a9e3bbc1e791cfc1bb5449e07562d9c07bb51a
akif171 <akifali229@gmail.com>
2024-11-02 15:52:57 +0500

    fixes ui

--- src/components/hook-form/rhf-date-picker.jsx
+++ src/components/hook-form/rhf-date-picker.jsx
@@ -9,6 +9,8 @@ export const RHFDatePicker = ({ name, onChange, value, rangeColors }) => {
   const { control } = useFormContext();
   const [isMobile, setIsMobile] = useState(false);
 
+  console.log(value);
+
   useEffect(() => {
     const handleResize = () => {
       setIsMobile(window.innerWidth < 930);
@@ -32,7 +34,7 @@ export const RHFDatePicker = ({ name, onChange, value, rangeColors }) => {
           ranges={value}
           direction={isMobile ? "vertical" : "horizontal"}
           rangeColors={rangeColors}
-          className="w-fit z-20"
+          className="w-fit z-20 bg-white"
           calendarWrapper="mb-10 "
         />
       )}
