3a226d202b29409bd946001632374507fab4cbc5
akif171 <akifali229@gmail.com>
2024-12-07 12:28:41 +0500

    added env in gitignore

--- src/components/hook-form/rhf-input.jsx
+++ src/components/hook-form/rhf-input.jsx
@@ -1,121 +1,121 @@
-"use client";
-import React from "react";
-import { Controller, useFormContext } from "react-hook-form";
-import { Iconify } from "../iconify";
-import { Typography } from "../typography";
-import { cn } from "@/src/libs/cn";
-import { useState } from "react";
-import get from "lodash/get";
+  "use client";
+  import React from "react";
+  import { Controller, useFormContext } from "react-hook-form";
+  import { Iconify } from "../iconify";
+  import { Typography } from "../typography";
+  import { cn } from "@/src/libs/cn";
+  import { useState } from "react";
+  import get from "lodash/get";
 
-export const RHFInput = React.memo(
-  ({
-    label,
-    name,
-    placeholder,
-    disabled = false,
-    className,
-    startIcon,
-    errorClass,
-    startIconClass,
-    inputClass,
-    customInputClass,
-    endIconClass,
-    endIcon,
-    onChange,
-    readOnly = false,
-    type = "text",
-  }) => {
-    const {
-      control,
-      formState: { errors },
-    } = useFormContext();
-    const [showPassword, setShowPassword] = useState(false);
+  export const RHFInput = React.memo(
+    ({
+      label,
+      name,
+      placeholder,
+      disabled = false,
+      className,
+      startIcon,
+      errorClass,
+      startIconClass,
+      inputClass,
+      customInputClass,
+      endIconClass,
+      endIcon,
+      onChange,
+      readOnly = false,
+      type = "text",
+    }) => {
+      const {
+        control,
+        formState: { errors },
+      } = useFormContext();
+      const [showPassword, setShowPassword] = useState(false);
 
-    const togglePasswordVisibility = () => {
-      setShowPassword((prev) => !prev);
-    };
+      const togglePasswordVisibility = () => {
+        setShowPassword((prev) => !prev);
+      };
 
-    const isPasswordField = type === "password";
+      const isPasswordField = type === "password";
 
-    return (
-      <Controller
-        name={name}
-        control={control}
-        render={({ field }) => (
-          <div
-            className={cn(
-              "relative flex flex-col gap-1 w-full",
-              className,
-              disabled && "cursor-not-allowed"
-            )}
-          >
-            {label && (
-              <Typography
-                variant="p"
-                className={`text-custom-black !text-sm bg-white absolute -top-2.5 left-3 ${
-                  disabled ? "opacity-50" : ""
-                }`}
-              >
-                {label}
-              </Typography>
-            )}
+      return (
+        <Controller
+          name={name}
+          control={control}
+          render={({ field }) => (
             <div
               className={cn(
-                "flex items-center rounded bg-white h-12 px-2 gap-2 border border-custom-neutral",
-                inputClass,
-                disabled && "!bg-gray-100 cursor-not-allowed"
+                "relative flex flex-col gap-1 w-full",
+                className,
+                disabled && "cursor-not-allowed"
               )}
             >
-              {startIcon && (
-                <Iconify
-                  iconName={startIcon}
-                  className={`${startIconClass} !w-5 !h-5`}
-                />
+              {label && (
+                <Typography
+                  variant="p"
+                  className={`text-custom-black !text-sm bg-white absolute -top-2.5 left-3 ${
+                    disabled ? "opacity-50" : ""
+                  }`}
+                >
+                  {label}
+                </Typography>
               )}
+              <div
+                className={cn(
+                  "flex items-center rounded bg-white h-12 px-2 gap-2 border border-custom-neutral",
+                  inputClass,
+                  disabled && "!bg-gray-100 cursor-not-allowed"
+                )}
+              >
+                {startIcon && (
+                  <Iconify
+                    iconName={startIcon}
+                    className={`${startIconClass} !w-5 !h-5`}
+                  />
+                )}
 
-              <input
-                type={isPasswordField && showPassword ? "text" : type}
-                {...field}
-                value={field.value || ""}
-                readOnly={readOnly}
-                autoComplete="off"
-                onChange={(e) => {
-                  field.onChange(e);
-                  onChange?.(e); 
-                }}
-                placeholder={placeholder}
-                className={`w-full text-sm rounded-md outline-none px-2 placeholder:text-neutral-300 text-secondary bg-transparent ${customInputClass} ${className} ${
-                  disabled ? "pointer-events-none" : ""
-                }`}
-              />
-
-              {isPasswordField && (
-                <Iconify
-                  iconName={
-                    showPassword
-                      ? "ic:round-visibility"
-                      : "ic:round-visibility-off"
-                  }
-                  className={`cursor-pointer text-black ${endIconClass}`}
-                  onClick={togglePasswordVisibility}
+                <input
+                  type={isPasswordField && showPassword ? "text" : type}
+                  {...field}
+                  value={field.value || ""}
+                  readOnly={readOnly}
+                  autoComplete="off"
+                  onChange={(e) => {
+                    field.onChange(e);
+                    onChange?.(e); 
+                  }}
+                  placeholder={placeholder}
+                  className={`w-full text-sm rounded-md outline-none px-2 placeholder:text-neutral-300 text-secondary bg-transparent ${customInputClass} ${className} ${
+                    disabled ? "pointer-events-none" : ""
+                  }`}
                 />
-              )}
 
-              {endIcon && (
-                <Iconify iconName={endIcon} className={endIconClass} />
+                {isPasswordField && (
+                  <Iconify
+                    iconName={
+                      showPassword
+                        ? "ic:round-visibility"
+                        : "ic:round-visibility-off"
+                    }
+                    className={`cursor-pointer text-black ${endIconClass}`}
+                    onClick={togglePasswordVisibility}
+                  />
+                )}
+
+                {endIcon && (
+                  <Iconify iconName={endIcon} className={endIconClass} />
+                )}
+              </div>
+              {errors && (
+                <Typography
+                  variant="p"
+                  className={`!text-xs text-red-400 transition-all duration-500 ${errorClass}`}
+                >
+                  {get(errors, name)?.message}
+                </Typography>
               )}
             </div>
-            {errors && (
-              <Typography
-                variant="p"
-                className={`!text-xs text-red-400 transition-all duration-500 ${errorClass}`}
-              >
-                {get(errors, name)?.message}
-              </Typography>
-            )}
-          </div>
-        )}
-      />
-    );
-  }
-);
+          )}
+        />
+      );
+    }
+  );
